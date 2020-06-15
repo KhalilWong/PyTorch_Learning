4d431fba41e0b183bfd13689b0de7a7c21f67ca5
Khalil Wong <1039906090@qq.com>
2020-06-15 20:38:30 +0800

    one GPU

--- main_test2.py
+++ main_test2.py
@@ -11,20 +11,22 @@ import numpy as np
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 8, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(8, 32, 5)
+        self.fc1 = nn.Linear(32 * 5 * 5, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 32 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 ################################################################################
@@ -38,7 +40,10 @@ def imshow(img):
 ################################################################################
 def Training(trainloader):
     #
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
     net = Net()
+    net.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     for epoch in range(2):                                                      # loop over the dataset multiple times
@@ -46,7 +51,7 @@ def Training(trainloader):
         for i, data in enumerate(trainloader, 0):
             # get the inputs
             inputs, labels = data
-
+            inputs, labels = inputs.to(device), labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
 
@@ -125,7 +130,7 @@ def main_test2():
     # 打印图片标签
     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
     #
-    #Training(trainloader)
+    Training(trainloader)
     #
     Test(testloader, './cifar_net.pth', classes)
 
